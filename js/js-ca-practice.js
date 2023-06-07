db2337b25e157e47294d4d0c76f48f0abe01554a
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2023-06-07 14:06:40 +0200

    added stylesheet

--- js/js-ca-practice.js
+++ js/js-ca-practice.js
@@ -14,27 +14,3 @@ const coffemaker = {
     }
 }
 
-//read.me # Coffemaker
-
-//This object has the following properties:
-
-// - Brand (string), is the manufactor of the object
-// - Volume (number), is the capacity in litres
-// - Price (number), is what the object costs in Norwegian kroners
-// - Weight (number), is what the object weights in kilograms
-// - On (boolian), when boolian "on" is false, the coffee maker is  not turned on
-// - Color (array), is the different colours the coffee maker is made in
-// - Ignition (function), the coffee maker can be turned on or off
-
-
-const television = {
-    brand: "samsung"
-    size: 32
-    weight: 
-    on: false
-    color: ["black", "silver", "white"]
-    chanel: ["tv3", "tv2", "nrk", "disney", "discovery"]
-    ignition: function () {
-        television.on = !television.on
-    }
-}
---
+++ js/television.js
@@ -0,0 +1,12 @@
+
+const television = {
+    brand: "samsung"
+    size: 32
+    weight: 
+    on: false
+    color: ["black", "silver", "white"]
+    chanel: ["tv3", "tv2", "nrk", "disney", "discovery"]
+    ignition: function () {
+        television.on = !television.on
+    }
+}
