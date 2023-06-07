23bccaa18ffd6a95b9e4e636b49959a64ba9d646
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2023-06-07 09:23:30 +0200

    fineal changes done for the ca

--- js/js-ca-practice.js
+++ js/js-ca-practice.js
@@ -18,14 +18,6 @@ const coffemaker = {
 
 //This object has the following properties:
 
-// - Brand (string), is the manufactor of the object
-// - Volume (number), is the capacity in litres
-// - Price (number), is what the object costs in norwegian kroners
-// - Weight (number), is what the object weights in kilograms
-// - On (boolian)
-// - Color (array), different colors on the coffe maker
-// - Ignition (funtion), the coffe maker can be on or off
-
 
 
 
